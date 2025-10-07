5cadefbccbc1792ea3dc180a026b2239a9e2dff9
gabrielfst30 <gabrielfst30@gmail.com>
2025-10-07 01:09:50 -0300

    refactor: traduzindo comentarios para portugues

--- src/OurToken.sol
+++ src/OurToken.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+pragma solidity ^0.8.18;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract OurToken is ERC20 {
-    // The constructor mints an initial supply of tokens to the deployer's address
+    // O construtor minteia um fornecimento inicial de tokens para o endereço do deployer
     constructor(uint256 initialSupply) ERC20("OurToken", "OTT") {
-        // Mint the initial supply to the contract deployer
+        // Minteia o fornecimento inicial para o deployer do contrato
         _mint(msg.sender, initialSupply);
     }
 }
