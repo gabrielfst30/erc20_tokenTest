c3eed32de22eedbb1cbcc5649bea2a22ee509143
gabrielfst30 <gabrielfst30@gmail.com>
2025-10-07 01:09:26 -0300

    feature: retornando OurToken e definindo um supply inicial de 1000 ether

--- script/DeployOurToken.s.sol
+++ script/DeployOurToken.s.sol
@@ -8,9 +8,10 @@ contract DeployOurToken is Script {
     // Initial supply of 100 tokens with 18 decimals
     uint256 public constant INITIAL_SUPPLY = 1000 ether;
 
-    function run() external {
+    function run() external returns (OurToken) {
         vm.startBroadcast();
-        new OurToken(INITIAL_SUPPLY); // Deploy the OurToken contract with initial supply
+        OurToken token = new OurToken(INITIAL_SUPPLY); // Deploy the OurToken contract with initial supply
         vm.stopBroadcast();
+        return token; // Return the deployed token contract instance
     }
 }
